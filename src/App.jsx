84e3dae058752a5489f8a6ba3c8b4bf69082dcd5
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-01-10 00:20:33 +0100

    Corrección: Configuración adecuada de rutas en App.jsx para renderizar correctamente el componente Navbar en la ruta '/'

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,20 @@
 //------------------
 
 import React from 'react'
-import { BrowserRouter, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from './components/molecules/Navbar'
 
 //------------------
 //BrowserRouter
 //------------------
 
 // Recuerda qué:
-// <BrowserRouter> envuelve toda nuestra aplicación, este es el enrutaador que conecta nuestra UI con el URL del navegador. (Siempre debe de escribirse de esta forma), cuando decidais escribir las rutas de las moleculas que creemos, lo hareís dentro de <Routes> primero con path="/" y luego con el elemento que hayamos creado element={<Home />}</Routes>, Recordad que al llamar a nuestro elemento, se debe de importar aútomáticamente en la cabecera de nuestro archivo App.jsx.
+// <BrowserRouter> envuelve toda nuestra aplicación, este es el enrutador que conecta nuestra UI con el URL del navegador. (Siempre debe de escribirse de esta forma), cuando decidais escribir las rutas de las moleculas que creemos, lo hareís dentro de <Routes> <Route> primero con path="/" y luego con el elemento que hayamos creado element={<Home />}</Route></Routes>, Recordad que al llamar a nuestro elemento, se debe de importar aútomáticamente en la cabecera de nuestro archivo App.jsx.
 const App = () => (
   <BrowserRouter>
-    <Routes></Routes>
+    <Routes>
+      <Route path="/" element={<Navbar />} />
+    </Routes>
   </BrowserRouter>
 )
 
