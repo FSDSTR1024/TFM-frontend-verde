e478824ca91964777e4bbd9ee63ab070af73bc8f
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-01-22 01:06:09 +0100

    The button has been created

--- src/stories/components/atoms/Button.jsx
+++ src/stories/components/atoms/Button.jsx
@@ -1,12 +1,17 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+
 import './Button.css'
-const Button = ({ children }) => {
-  return <button>{children}</button>
+
+const getStyle = (...args) => ['button', ...args].filter(Boolean).join(' ')
+
+const Button = ({ children, type }) => {
+  return <button className={getStyle(type)}>{children}</button>
 }
 
 Button.propTypes = {
   children: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(['primary', 'secondary', 'tertiary']),
 }
 
 export default Button
