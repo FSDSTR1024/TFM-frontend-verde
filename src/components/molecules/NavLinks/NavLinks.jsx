8095e18d24470e0e42039c3d3a74e5ff1f89bef5
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-01-24 04:07:45 +0100

    Probando botones y diseños

--- src/components/molecules/NavLinks/NavLinks.jsx
+++ src/components/molecules/NavLinks/NavLinks.jsx
@@ -1,21 +1,24 @@
 // NavLinks.jsx (Versión corregida)
-import Button from '@components/atoms/Button' // ✔️ Importa tus botones
-import '@components/molecules/NavLinks/NavLinks.css'
+import Button from '@components/atoms/Button'
+import '@components/molecules/NavLinks/NavLinks.css' // ✔️ CSS estándar
 import React from 'react'
 
-// NavLinks.jsx
+const NAV_ITEMS = ['SHARE', 'ABOUT', 'THE FILMS', 'PRESS', 'EDUCATION']
+
 const NavLinks = () => (
-  <ul className="nav-links">
-    <li>
-      <Button variant="nav">SHARE</Button>
-    </li>
-    <li>
-      <Button variant="nav">ABOUT</Button>
-    </li>
-    <li>
-      <Button variant="nav">THE FILMS</Button>
-    </li>
-  </ul>
+  <nav className="nav-container">
+    {' '}
+    {/* Clase normal */}
+    <ul className="nav-list">
+      {' '}
+      {/* Clase normal */}
+      {NAV_ITEMS.map((item) => (
+        <li key={item}>
+          <Button type="nav">{item}</Button>
+        </li>
+      ))}
+    </ul>
+  </nav>
 )
 
 export default NavLinks
