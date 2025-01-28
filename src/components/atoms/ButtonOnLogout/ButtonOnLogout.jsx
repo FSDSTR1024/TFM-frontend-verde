59a7720a56cefb70a74cdc592313e9cfedccc929
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-01-28 02:45:47 +0100

    ButtonOnLogout has been created

---
+++ src/components/atoms/ButtonOnLogout/ButtonOnLogout.jsx
@@ -0,0 +1,24 @@
+import Button from '@components/atoms/Button'
+import PropTypes from 'prop-types'
+import styles from './ButtonOnLogout.module.css'
+
+const ButtonOnLogout = ({ children, onClick = () => {}, ariaLabel }) => {
+  return (
+    <Button
+      type='secondary'
+      onClick={e => onClick && onClick(e)}
+      ariaLabel={ariaLabel}
+      className={styles.logout}
+    >
+      {children}
+    </Button>
+  )
+}
+
+ButtonOnLogout.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func,
+  ariaLabel: PropTypes.string.isRequired
+}
+
+export default ButtonOnLogout
