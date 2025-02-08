93252de11530af43feb2d87913ad84b98d27988d
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-02-08 21:32:57 +0100

    Aún se están corrigiendo errores venidos de la migración

--- src/Components/organisms/Header/Header.jsx
+++ src/Components/organisms/Header/Header.jsx
@@ -2,8 +2,12 @@ import Logo from '@components/atoms/Logo'
 import NavLinks from '@components/molecules/NavLinks'
 
 const Header = () => (
-  <header className="relative w-full min-h-[60px] max-h-[80px] flex justify-between items-center gap-5 bg-white box-border px-8 md:px-6 lg:px-10">
-    <Logo />
+  <header className="relative w-full min-h-[60px] max-h-[80px] flex justify-between items-center bg-white box-border">
+    <div className="flex items-start">
+      {' '}
+      {/* Forzamos la alineación en la esquina superior izquierda */}
+      <Logo />
+    </div>
     <NavLinks />
     {/* Línea de borde inferior */}
     <div className="absolute bottom-0 left-1/2 w-[calc(100%-4rem)] h-[1px] bg-primary-dark/15 opacity-0 transition-opacity duration-300 hover:opacity-100"></div>
--- src/Components/organisms/Navbar/Navbar.jsx
+++ src/Components/organisms/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import Header from '@components/organisms/Header'
 
 const Navbar = () => (
-  <nav className="bg-white shadow-md px-8 md:px-4 z-10">
+  <nav className="bg-white shadow-md">
     <Header />
   </nav>
 )
