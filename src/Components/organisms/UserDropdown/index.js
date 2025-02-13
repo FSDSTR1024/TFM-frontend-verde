8a6e29d6050531e830cc380a37b23ae27f2282b6
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-02-13 21:46:42 +0100

    Se empieza a desarrollar toda la lógica detras de UserDropdown, se separa por componentes dentro de la misma carpeta de Dropown. De esta forma se evita la sobrecarga de un solo componente, dando como resultado mayor modulariad y sencillez.

---
+++ src/Components/organisms/UserDropdown/index.js
@@ -0,0 +1,4 @@
+export { default as UserDropdown } from './UserDropdown'
+export { default as ProfileForm } from './ProfileForm'
+export { default as WalletSection } from './WalletSection'
+export { default as OrdersSection } from './OrdersSection'
