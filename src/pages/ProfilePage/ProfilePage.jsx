d6166a49adb0dc22a251f2acce7c35d796a5094a
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-03-18 20:30:38 +0100

    Ahora todos los componentes que dependen del contexto global funcionan correctamente const { user, login } = useAuth()se ha convertido en un código más manejable y mas accesible, ningún componente marca error, profile page ahora recoge  formDataImage.append('cloud_name', '*******') para enviar correctamente el formulario imagen correctamente a Cloudinary, todo el código queda depurado, por lo tanto se creara otra rama donde el código este completamente limpio de console.log que puedan poner en riesgo las credenciales de los usuuarios

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -128,6 +128,7 @@ const ProfilePage = () => {
     const formDataImage = new FormData()
     formDataImage.append('file', file) // Clave esperada por Cloudinary
     formDataImage.append('upload_preset', 'user_profile') // Configuración de Cloudinary
+    formDataImage.append('cloud_name', 'dwsnf2wlr') // Configuración de Cloudinary
 
     setLoading(true)
     try {
