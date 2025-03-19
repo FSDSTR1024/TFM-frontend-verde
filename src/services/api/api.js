8d10ad5ec6124553389e5ac084b0941b90926485
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-03-19 22:17:01 +0100

    Se corrige la variable de entorno y se deja configurada exclusivamente para el entorno de producción

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -1,14 +1,13 @@
 import axios from 'axios'
 
 
-// Primero obtenemos la URL de la variable de entorno para producción y después la URL predeterminada para trabajar en local
+// En esta rama mantenemos la URL del entorno de producción puesto que la ruta del entorno de desarrollo supone una interferencia en el login del usuario y este no puede hacer un correcto logout, Si en el entonorno de producción se genera el mismo fallo, entonces se debe actuar sobre el componente authController (logout) y ver exactamente que puede generar el fallo en producción.
 const API_BASE_URL =
   import.meta.env.VITE_API_URL ||
-  'https://tfm-backend-verde.onrender.com' ||
-  'http://localhost:3000'
+  'https://tfm-backend-verde.onrender.com'
 
 const api = axios.create({
-  baseURL: API_BASE_URL, // Utilizamos 
+  baseURL: API_BASE_URL, // Utilizamos
   withCredentials: true,
   timeout: 5000,
   headers: {
