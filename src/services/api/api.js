ca7487b5f95d00a441d98b934f4f19a7b7e12244
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-03-19 22:13:28 +0100

    api.js se mantiene en un entorno de desarrollo para que se pueda llevar a cabo las pruebas en el localhost:3000

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -1,14 +1,10 @@
 import axios from 'axios'
 
-
-// Primero obtenemos la URL de la variable de entorno para producción y después la URL predeterminada para trabajar en local
-const API_BASE_URL =
-  import.meta.env.VITE_API_URL ||
-  'https://tfm-backend-verde.onrender.com' ||
-  'http://localhost:3000'
+// En esta rama mantenemos la URL del entorno de desarrollo puesto que la ruta del entorno de producción supone una interferencia en el login del usuario y este no puede hacer un correcto logout, Si en el entonorno de producción se genera el mismo fallo, entonces se debe actuar sobre el componente authController (logout) y ver exactamente que puede generar el fallo en producción.
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
 
 const api = axios.create({
-  baseURL: API_BASE_URL, // Utilizamos 
+  baseURL: API_BASE_URL, // Utilizamos
   withCredentials: true,
   timeout: 5000,
   headers: {
