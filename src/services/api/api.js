6d8dc8c3129414f47bac3573b6f24d2f9b6783c5
Andrés Guerrero <andresdavid.g.r2898@gmail.com>
2025-03-26 00:10:30 +0100

    Subo el timeOut para despertar el servidor de Render despuésde un tiempo de inactividad

--- src/services/api/api.js
+++ src/services/api/api.js
@@ -6,7 +6,7 @@ const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
 const api = axios.create({
   baseURL: API_BASE_URL, // Utilizamos
   withCredentials: true,
-  timeout: 5000,
+  timeout: 15000,
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
